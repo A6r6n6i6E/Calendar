b1ba9fb0bdae001a51db74df98a45635dc078866
A6r6n6i6E <114140838+A6r6n6i6E@users.noreply.github.com>
2026-09-02 10:51:59 +0200

    Update build.mjs

--- build.mjs
+++ build.mjs
@@ -29,10 +29,8 @@ await Promise.all([
   copyClient(staticOutputRoot),
   copyClient(join(workerOutputRoot, "client")),
   mkdir(join(workerOutputRoot, "server"), { recursive: true }),
-  mkdir(join(workerOutputRoot, ".openai"), { recursive: true }),
 ]);
 await Promise.all([
   cp(join(projectRoot, "worker", "index.js"), join(workerOutputRoot, "server", "index.js")),
-  cp(join(projectRoot, ".openai", "hosting.json"), join(workerOutputRoot, ".openai", "hosting.json")),
 ]);
 console.log("Gotowa aplikacja: build/ (statyczna) i dist/ (z synchronizacją)");
